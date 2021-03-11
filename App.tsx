fa2511de884b62aab7e179a6ccfcee2540220394
b-haytham <boussarsarhaytham@gmail.com>
2021-03-11 23:28:27 +0100

    initial commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <>
+      <Text>Hello</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
